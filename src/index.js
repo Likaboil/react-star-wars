1c54b65051eb91d01ac8efcb8292b779b9423662
Liliya Sagitova <guverboil@gmail.com>
2021-01-27 18:06:41 +0300

    feat(api): Create class Service for API
    SAsunc service for use swapi.dev with async method getResource.
    Uses fetch API
    Return const res as result, contains json-file.
    Foreseen check for case of any error status

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log(`Star Wars DataBank`);
+class SwapiService {
+
+  async getResource(url) {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error (`Could not fetch %{url} , received ${res.status}`)
+    }
+
+    return res.json();
+  }
+}
