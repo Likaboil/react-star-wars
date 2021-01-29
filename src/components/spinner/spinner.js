b19a48d074d63c4851573497c3621cab77636502
Liliya Sagitova <guverboil@gmail.com>
2021-01-29 14:55:23 +0300

    feat(components) Add component Spinner
    Until loading ended Spinner will show instead Planet info

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './spinner.css';
+
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-ripple-eil3g1xl4lu">
+      <div class="ldio-bpauznwbn4m">
+        <div />
+        <div />
+    </div></div>
+  );
+};
+
+export default Spinner
