97ce9f95b9c9a11126f2b0dc827e854653d9330d
Liliya Sagitova <guverboil@gmail.com>
2021-01-29 10:47:19 +0300

    fix(randomPlanet): Add state props
    Use state props for render components

--- src/components/planet-random/planet-random.js
+++ src/components/planet-random/planet-random.js
@@ -4,27 +4,37 @@ import './planet-random.css';
 
 export default class PlanetRandom extends Component {
 
+  state = {
+    name: null,
+    population: null,
+    rotationPeriod: null,
+    diameter: null
+  }
+
   render() {
+
+    const {name, population, rotationPeriod, diameter} = this.state;
+
     return (
       <div className="planet-random jumbotron rounded">
         <img className="planet-image"
              src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-            alt="Planet Name"
+            alt={name}
              />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
