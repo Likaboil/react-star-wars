15bf05588da7e0db386e305b78bd9709816a7693
Liliya Sagitova <guverboil@gmail.com>
2021-01-29 15:18:59 +0300

    refactor(load): WP Move elements to own components
    Refactor elements for planet info with React Fragment. Two simple
    elements no needs to be a separate component.

--- src/components/planet-random/planet-random.js
+++ src/components/planet-random/planet-random.js
@@ -32,41 +32,43 @@ export default class PlanetRandom extends Component {
   };
 
   render() {
-
-    const {planet: {id, name, population, rotationPeriod, diameter},
-          loading} = this.state;
+    const {planet, loading} = this.state;
 
     return (
       <div className="planet-random jumbotron rounded">
-        <Spinner />
+        <PlanetView planet={planet} />
       </div>
     );
 
-    // return (
-    //   <div className="planet-random jumbotron rounded">
-    //     <img className="planet-image"
-    //          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-    //          alt={name}
-    //          />
-    //     <div>
-    //       <h4>{name}</h4>
-    //       <ul className="list-group list-group-flush">
-    //         <li className="list-group-item">
-    //           <span className="term">Population</span>
-    //           <span>{population}</span>
-    //         </li>
-    //         <li className="list-group-item">
-    //           <span className="term">Rotation Period</span>
-    //           <span>{rotationPeriod}</span>
-    //         </li>
-    //         <li className="list-group-item">
-    //           <span className="term">Diameter</span>
-    //           <span>{diameter}</span>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-
-    // );
   }
+}
+
+const PlanetView = ({planet}) => {
+  const {id, name, population, rotationPeriod, diameter} = planet;
+
+    return (
+      <React.Fragment>
+        <img className="planet-image"
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+             alt={name}
+             />
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
+      </React.Fragment>
+    );
 }
