8ec26cae7b0f92025f81d4ff1696d5896bac84e4
Liliya Sagitova <guverboil@gmail.com>
2021-02-03 13:52:21 +0300

    refactor: WP Add temporary item lists
    To show how to reuse the ItemList component when rendering
     list of planets or starships

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './app.css';
 
+import SwapiService from '../../api';
 import Header from '../header';
 import PlanetRandom from '../planet-random';
 import ErrorButton from '../error-button';
 import PeoplePage from '../people-page';
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
 
 
 export default class App extends Component {
+
+  swapiService = new SwapiService()
 
   state = {
     showRandomPlanet: true,
@@ -36,6 +41,26 @@ export default class App extends Component {
           <ErrorButton />
         </div>
         <PeoplePage />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}
+             />
+          </div>
+        </div>
       </div>
     );
   }
