d7a5381bb8c195877bed5bffb5d9c43fbf0ec332
Liliya Sagitova <guverboil@gmail.com>
2021-02-05 17:21:38 +0300

    refactor(error): Wrap ErorBoundry around all app
    To catch possible errors at App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,7 @@ import Header from '../header';
 import PlanetRandom from '../planet-random';
 import ErrorButton from '../error-button';
 import PeoplePage from '../people-page';
+import ErrorBoundry from '../error-boundry/';
 
 export default class App extends Component {
 
@@ -27,18 +28,20 @@ export default class App extends Component {
     const planet = this.state.showRandomPlanet ? <PlanetRandom /> :  null;
 
     return (
-      <div className="stardb-app">
-        <Header />
-        {planet}
-        <div className="row mb2 button-row">
-          <button className="toggle-planet btn btn-warning btn-lg"
-            onClick={this.toggleRandomPlanet} >
-            Toggle Random Planet
-          </button>
-          <ErrorButton />
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
+          {planet}
+          <div className="row mb2 button-row">
+            <button className="toggle-planet btn btn-warning btn-lg"
+              onClick={this.toggleRandomPlanet} >
+              Toggle Random Planet
+            </button>
+            <ErrorButton />
+          </div>
+          <PeoplePage />
         </div>
-        <PeoplePage />
-      </div>
+      </ErrorBoundry>
     );
   }
 };
