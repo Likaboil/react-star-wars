d5aaa02ab5a27225280f410d2972679802d3398c
Liliya Sagitova <guverboil@gmail.com>
2021-02-10 16:24:21 +0300

    feat: Add Switch to render 404
    If user tries  to get page that is not in app, he will redirect to
    "Page not found".

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import './app.css';
 
 import {
@@ -54,27 +58,31 @@ export default class App extends Component {
             <div className="stardb-app">
               <Header  onServiceChange={this.onServiceChange}/>
               <PlanetRandom />
-              <Route path="/" exact
-                     render={() => <h2>Welcome to StarDB</h2>} />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetPage} />
-              <Route path="/starships" exact component={StarshipsPage} />
-              <Route path="/starships/:id"
-                render={({match}) => {
-                  const {id} = match.params;
+              <Switch>
+                <Route path="/" exact
+                      render={() => <h2>Welcome to StarDB</h2>} />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route path="/starships/:id"
+                  render={({match}) => {
+                    const {id} = match.params;
 
-                  return (<StarshipDetails itemId={id}/>)
-                }} />
-              <Route path="/login"
-                render={() => (
-                  <LoginPage isLoggedIn={isLoggedIn}
-                  onLogin={this.onLogin}
-                  />
-                )} />
-              <Route path="/secret"
-                render={() => (
-                  <SecretPage isLoggedIn={isLoggedIn}/>
-                )} />
+                    return (<StarshipDetails itemId={id}/>)
+                  }} />
+                <Route path="/login"
+                  render={() => (
+                    <LoginPage isLoggedIn={isLoggedIn}
+                    onLogin={this.onLogin}
+                    />
+                  )} />
+                <Route path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn}/>
+                  )} />
+
+                <Route render={() => <h2>Page not found</h2>} />
+              </Switch>
             </div>
           </Router>
         </SwapiServiceProvider>
