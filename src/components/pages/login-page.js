702fb5c67b787b9776250743402b3f8324e606a0
Liliya Sagitova <guverboil@gmail.com>
2021-02-10 15:51:35 +0300

    refactor(auth): Fix LoginPage for param
    If logged will show "Logged"

--- src/components/pages/login-page.js
+++ src/components/pages/login-page.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const LoginPage = () => {
+const LoginPage = ({isLoggedIn, onLogin}) => {
+
+  if (isLoggedIn) {
+    return <p> Logged</p>
+  }
 
   return (
     <div className="jumbotron">
       <p>Login to see secret page!</p>
       <button
-        className="btn btn-primary">
+        className="btn btn-primary"
+        onClick={onLogin}>
         Login
       </button>
     </div>
